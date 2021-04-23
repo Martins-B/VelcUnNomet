7d66f44a2ca5e3fb505b76ad1c6203a737a0d80a
Martins-B <[martinsbardulis@gmail.com]>
2021-04-23 15:06:03 +0300

    Nodrošināta visu objektu novietošana un skaņas atskaņošana

--- Assets/Skripti/NomesanasVieta.cs
+++ Assets/Skripti/NomesanasVieta.cs
@@ -47,6 +47,30 @@ public class NomesanasVieta : MonoBehaviour, IDropHandler
                         case "Skola":
                             objektuSkripts.skanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[3]);
                         break;
+                        case "b2":
+                            objektuSkripts.skanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[4]);
+                        break;
+                        case "cements":
+                            objektuSkripts.skanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[5]);
+                        break;
+                        case "e46":
+                            objektuSkripts.skanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[6]);
+                        break;
+                        case "eskavators":
+                            objektuSkripts.skanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[7]);
+                        break;
+                        case "policija":
+                            objektuSkripts.skanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[8]);
+                        break;
+                        case "traktors1":
+                            objektuSkripts.skanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[9]);
+                        break;
+                        case "traktors2":
+                            objektuSkripts.skanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[10]);
+                        break;
+                        case "ugunsdzeseji":
+                            objektuSkripts.skanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[11]);
+                        break;
 
                         default:
                             Debug.Log("Nedefinēts tags!");
@@ -67,6 +91,30 @@ public class NomesanasVieta : MonoBehaviour, IDropHandler
                         break;
                         case "Skola":
                              objektuSkripts.autobuss.GetComponent<RectTransform>().localPosition = objektuSkripts.bussKoord;
+                        break;
+                        case "b2":
+                             objektuSkripts.b2.GetComponent<RectTransform>().localPosition = objektuSkripts.b2Koord;
+                        break;
+                        case "cements":
+                             objektuSkripts.cementaMasina.GetComponent<RectTransform>().localPosition = objektuSkripts.cementaKoord;
+                        break;
+                        case "e46":
+                             objektuSkripts.e46.GetComponent<RectTransform>().localPosition = objektuSkripts.e46Koord;
+                        break;
+                        case "eskavators":
+                             objektuSkripts.eskavators.GetComponent<RectTransform>().localPosition = objektuSkripts.eskavatorsKoord;
+                        break;
+                        case "policija":
+                             objektuSkripts.policija.GetComponent<RectTransform>().localPosition = objektuSkripts.policijaKoord;
+                        break;
+                        case "traktors1":
+                             objektuSkripts.traktors1.GetComponent<RectTransform>().localPosition = objektuSkripts.traktors1Koord;
+                        break;
+                        case "traktors2":
+                             objektuSkripts.traktors2.GetComponent<RectTransform>().localPosition = objektuSkripts.traktors2Koord;
+                        break;
+                        case "ugunsdzeseji":
+                             objektuSkripts.ugunsdzeseji.GetComponent<RectTransform>().localPosition = objektuSkripts.ugunsdzesejiKoord;
                         break;
 
                         default:
--- Assets/Skripti/Objekti.cs
+++ Assets/Skripti/Objekti.cs
@@ -7,6 +7,14 @@ public class Objekti : MonoBehaviour
     public GameObject atkritumuMasina;
     public GameObject atroMasina;
     public GameObject autobuss;
+    public GameObject b2;
+    public GameObject cementaMasina;
+    public GameObject e46;
+    public GameObject eskavators;
+    public GameObject policija;
+    public GameObject traktors1;
+    public GameObject traktors2;
+    public GameObject ugunsdzeseji;
 
     [HideInInspector]
     public Vector2 atkrKoord;
@@ -14,6 +22,22 @@ public class Objekti : MonoBehaviour
     public Vector2 atroKoord;
     [HideInInspector]
     public Vector2 bussKoord;
+    [HideInInspector]
+    public Vector2 b2Koord;
+    [HideInInspector]
+    public Vector2 cementaKoord;
+    [HideInInspector]
+    public Vector2 e46Koord;
+    [HideInInspector]
+    public Vector2 eskavatorsKoord;
+    [HideInInspector]
+    public Vector2 policijaKoord;
+    [HideInInspector]
+    public Vector2 traktors1Koord;
+    [HideInInspector]
+    public Vector2 traktors2Koord;
+    [HideInInspector]
+    public Vector2 ugunsdzesejiKoord;
 
     public Canvas kanva;
     public AudioSource skanasAvots;
@@ -28,5 +52,13 @@ public class Objekti : MonoBehaviour
         atkrKoord = atkritumuMasina.GetComponent<RectTransform>().localPosition;
         atroKoord = atroMasina.GetComponent<RectTransform>().localPosition;
         bussKoord = autobuss.GetComponent<RectTransform>().localPosition;
+        b2Koord =  b2.GetComponent<RectTransform>().localPosition;
+        cementaKoord = cementaMasina.GetComponent<RectTransform>().localPosition;
+        e46Koord = e46.GetComponent<RectTransform>().localPosition;
+        eskavatorsKoord = eskavators.GetComponent<RectTransform>().localPosition;
+        policijaKoord = policija.GetComponent<RectTransform>().localPosition;
+        traktors1Koord = traktors1.GetComponent<RectTransform>().localPosition;
+        traktors2Koord = traktors2.GetComponent<RectTransform>().localPosition;
+        ugunsdzesejiKoord = ugunsdzeseji.GetComponent<RectTransform>().localPosition;
     }    
 }
