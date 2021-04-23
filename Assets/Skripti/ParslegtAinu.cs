feddd3e411b8b3c6e7cc54027990ae12baccecb5
Karlis Zeiburlins <karliszeibis@gmail.com>
2021-04-23 23:54:43 +0300

    Sākuma izvēlnes funkcijas ieviešana

---
+++ Assets/Skripti/ParslegtAinu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ParslegtAinu : MonoBehaviour
+{
+  public void speletSpeli()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void atgriezties()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
